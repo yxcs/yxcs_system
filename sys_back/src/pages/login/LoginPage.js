204e70c7c431447f8b137f5f12dfdca98100b1a8
yuxiaochao <yuxiaochao@zhuge.com>
2019-04-22 01:30:15 +0800

    登录form实现

--- sys_back/src/pages/login/LoginPage.js
+++ sys_back/src/pages/login/LoginPage.js
@@ -1,17 +1,58 @@
 import React, { Component } from 'react';
-
-
+import './login.less';
+import {
+  Form, Icon, Input, Button,
+} from 'antd';
 class LoginPage extends Component {
   constructor(props) {
     super(props);
   }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.props.form.validateFields((err, values) => {
+      if (!err) {
+        console.log('Received values of form: ', values);
+      }
+    });
+  }
+
   render() {
+    const { getFieldDecorator } = this.props.form;
     return (
-      <div>
-        login
+      <div className="login__bg">
+        <div className="login__box--container">
+          <div className="login__box--from">
+            <p className="title">管理员登录</p>
+            <Form onSubmit={this.handleSubmit} className="login-form">
+              <Form.Item>
+                {getFieldDecorator('userName', {
+                  rules: [{ required: true, message: '请输入用户名' }],
+                })(
+                  <Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Username" />
+                )}
+              </Form.Item>
+              <Form.Item>
+                {getFieldDecorator('password', {
+                  rules: [{ required: true, message: '请输入登录密码' }],
+                })(
+                  <Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="Password" />
+                )}
+              </Form.Item>
+              <Form.Item>
+                <Button type="primary" htmlType="submit" className="login-form-button">
+                  登录
+                </Button>
+                 <a href="">注册</a>
+              </Form.Item>
+            </Form>
+          </div>
+        </div>
       </div>
     )
   }
 }
 
-export default LoginPage;
+const WrappedNormalLoginForm = Form.create({ name: 'normal_login' })(LoginPage);
+
+export default WrappedNormalLoginForm;
