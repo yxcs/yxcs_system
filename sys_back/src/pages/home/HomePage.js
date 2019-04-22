022a895c78d9ebead7e31e5edd9efa468e34a431
yxcs <2741822345@qq.com>
2019-04-22 16:43:38 +0800

    后台界面整体布局搭建

---
+++ sys_back/src/pages/home/HomePage.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './home.less'
+
+class HomePage extends Component {
+
+  render () {
+
+    return (
+      <div>
+        home
+      </div>
+    )
+  }
+}
+
+export default HomePage
---
+++ sys_back/src/pages/layout/LayoutPage.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './layout.less'
+
+class LayoutPage extends Component {
+
+  render () {
+
+    return (
+      <div>
+        layout
+      </div>
+    )
+  }
+}
+
+export default LayoutPage
