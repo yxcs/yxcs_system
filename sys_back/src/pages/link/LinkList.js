cf30800755aa8ae9b964d291fc4c9634a5f01fcf
yxcs <2741822345@qq.com>
2019-06-03 21:04:33 +0800

    更改正则校验

--- sys_back/src/pages/link/LinkList.js
+++ sys_back/src/pages/link/LinkList.js
@@ -119,16 +119,7 @@ class LinkList extends Component {
   }
 
   validateWebsite = (rule, value, callback) => {
-    const strRegex = '^((https|http|ftp|rtsp|mms)?://)'
-      + '?(([0-9a-z_!~*\'().&=+$%-]+: )?[0-9a-z_!~*\'().&=+$%-]+@)?' //ftp的user@
-      + '(([0-9]{1,3}.){3}[0-9]{1,3}' // IP形式的URL- 199.194.52.184
-      + '|' // 允许IP和DOMAIN（域名）
-      + '([0-9a-z_!~*\'()-]+.)*' // 域名- www.
-      + '([0-9a-z][0-9a-z-]{0,61})?[0-9a-z].' // 二级域名
-      + '[a-z]{2,6})' // first level domain- .com or .museum
-      + '(:[0-9]{1,4})?' // 端口- :80
-      + '((/?)|' // a slash isn't required if there is no file name
-      + '(/[0-9a-z_!~*\'().;?:@&=+$,%#-]+)+/?)$'; 
+    const strRegex = /^(?:([A-Za-z]+):)?(\/{0,3})([0-9.\-A-Za-z]+)(?::(\d+))?(?:\/([^?#]*))?(?:\?([^#]*))?(?:#(.*))?$/; 
     const WEB_REG = new RegExp(strRegex); 
     if (!WEB_REG.test(value)) {
       callback('请输入正确的链接');
