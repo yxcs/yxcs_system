fe15172b7221e00e8a202aa4cd97639e8ec7a293
yxcs <2741822345@qq.com>
2019-06-17 16:54:41 +0800

    drysys

--- sys_front/src/config.js
+++
@@ -1,2 +0,0 @@
-export const apiUrl = 'http://localhost:8808'
-export const version = 'v1'
--- sys_front/src/services/api.js
+++
@@ -1,4 +0,0 @@
-import { apiUrl, version } from '../config';
-
-export const GETARTICLELIST = `${apiUrl}/${version}/getArticles`
-export const GETARTICLEDetails = `${apiUrl}/${version}/getArticle`
